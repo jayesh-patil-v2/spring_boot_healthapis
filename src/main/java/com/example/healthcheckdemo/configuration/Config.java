9e41a569ea1f5f691a4dd84adaea93cce6f24966
Suvam21 <suvambehera21@gmail.com>
2023-08-09 15:18:12 +0530

    added cors

---
+++ src/main/java/com/example/healthcheckdemo/configuration/Config.java
@@ -0,0 +1,14 @@
+package com.example.healthcheckdemo.configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class Config implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:3000")
+		.allowedMethods("GET", "POST", "PUT","DELETE");
+	}
+
+}
